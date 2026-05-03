e01c786a8c1686500f02ee11775f5ac33c212732
tag101 <tag101@hanmail.net>
2026-05-03 12:14:57 +0900

    58. Orders Microservice - 기능 구현 ①

--- new-toy-msa-ch03-1/service-discovery/src/main/java/com/dogmeeting/servicediscovery/ServiceDiscoveryApplication.java
+++ new-toy-msa-ch03-1/service-discovery/src/main/java/com/dogmeeting/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.example.servicediscovery;
+package com.dogmeeting.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- new-toy-msa-ch03-1/service-discovery/src/test/java/com/dogmeeting/servicediscovery/ServiceDiscoveryApplicationTests.java
+++ new-toy-msa-ch03-1/service-discovery/src/test/java/com/dogmeeting/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.servicediscovery;
+package com.dogmeeting.servicediscovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
