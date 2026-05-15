57a5b6cb9a48731c917a6f8ba1477ce3f8619862
tag101 <tag101@hanmail.net>
2026-05-15 13:03:15 +0900

    131. Users Microservice에 CircuitBreaker 적용

--- new-toy-msa-ch03-1/user-service/src/main/java/com/dogmeeting/userservice/service/UserServiceImpl.java
+++ new-toy-msa-ch03-1/user-service/src/main/java/com/dogmeeting/userservice/service/UserServiceImpl.java
@@ -9,6 +9,8 @@ import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
@@ -37,16 +39,21 @@ public class UserServiceImpl implements UserService {
 
     OrderServiceClient orderServiceClient;
 
+    CircuitBreakerFactory circuitBreakerFactory;
+
+
     public UserServiceImpl(Environment env
                         , UserRepository userRepository
                         , BCryptPasswordEncoder passwordEncoder
                         , RestTemplate restTemplate
-                        , OrderServiceClient orderServiceClient) {
+                        , OrderServiceClient orderServiceClient
+                        ,CircuitBreakerFactory circuitBreakerFactory) {
         this.env = env;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.restTemplate = restTemplate;
         this.orderServiceClient = orderServiceClient;
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     @Override
@@ -103,7 +110,11 @@ public class UserServiceImpl implements UserService {
 //            log.error(feignException.getMessage());
 //        }
         /* usgin a feignClient with feignErrorDecoder */
-        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+        // List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+        CircuitBreaker circuitBreaker =  circuitBreakerFactory.create("circuitbreaker");
+        List<ResponseOrder> orderList = circuitBreaker.run(() -> orderServiceClient.getOrders(userId),
+                throwable -> new ArrayList<>());
+
         userDto.setOrders(orderList);
 
         return userDto;
